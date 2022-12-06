d0ab613efabbd106b0b61388a9e56fc57b2565de
Salipa-Gurung <salipagurung@gmail.com>
2022-12-06 13:17:27 +0545

    deleting sub admin incomplete

--- frontend/tests/helperTests/deleteSubAdmin.js
+++ frontend/tests/helperTests/deleteSubAdmin.js
@@ -1,4 +1,16 @@
+const axios = require("axios");
 
- module.exports = function deleteSubAdmin(subAdminName){
-
+module.exports = async function deleteSubAdmin(subAdminName){
+ let response = "";
+ const token = `admin:admin`;
+ const encodedToken = Buffer.from(token).toString('base64');
+ response = await axios.post(`${baseUrl}/admin/`, {
+  "username":userDetails.username,
+  "password":userDetails.password
+ }, {
+  headers: {
+   "Authorization": 'Basic ' + encodedToken,
+   'Content-Type': 'application/json',
+  }
+ })
 }
