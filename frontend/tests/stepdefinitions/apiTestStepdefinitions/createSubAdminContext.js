3f922a7c00434c015e2c36a4c30694abf5dc860a
Salipa-Gurung <salipagurung@gmail.com>
2022-12-08 17:07:18 +0545

    implemented object of helper class

--- frontend/tests/stepdefinitions/apiTestStepdefinitions/createSubAdminContext.js
+++ frontend/tests/stepdefinitions/apiTestStepdefinitions/createSubAdminContext.js
@@ -1,32 +1,25 @@
 const { When, Then} = require("@cucumber/cucumber");
 const {expect} = require("@playwright/test");
 const axios = require("axios");
-let response = "";
-const baseUrl = "http://localhost:3001"
+const { Helper } = require('../../helperTests/apiHelper');
+
+const helper = new Helper();
 When('admin creates sub admin with following credentials', async function (dataTable) {
+    await helper.createSubAdmin(dataTable);
     const userDetails = dataTable.rowsHash();
-    const token = `admin:admin`;
-    const encodedToken = Buffer.from(token).toString('base64');
-        response = await axios.post(`${baseUrl}/admin/`, {
-            "username":userDetails.username,
-            "password":userDetails.password
-        }, {
-            headers: {
-                "Authorization": 'Basic ' + encodedToken,
-                'Content-Type': 'application/json',
-            }
-        })
-    subAdminCreated.push(userDetails.username)
+    subAdminCreated.push(userDetails.username);
 });
 
-Then('status code should be {int}', function (expectedStatuscode) {
-    const actualStatusCode = response.status;
-    console.log(actualStatusCode);
-    expect(expectedStatuscode).toBe(actualStatusCode);
-
+Then('status code should be {int}', function (expectedStatusCode) {
+    console.log(helper.response)
+    const actualStatusCode = helper.response.status;
+    // console.log(actualStatusCode)
+    // console.log(expectedStatusCode)
+    expect(expectedStatusCode).toBe(actualStatusCode);
 });
 
 Then('the created sub-admin username should be {string}', function (expectedUserName) {
-   const actualUserName = response.data.username;
+   // const actualUserName = response.data.username;
+    const actualUserName = helper.response.data.username;
    expect(expectedUserName).toBe(actualUserName);
 });
