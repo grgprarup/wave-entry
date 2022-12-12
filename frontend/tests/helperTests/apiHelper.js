4c32a674c35e31424b0c7c838381e70f33d16f0a
Salipa-Gurung <salipagurung@gmail.com>
2022-12-12 16:51:00 +0545

    added methods for admin login,update admin, create student,list student,list one student

--- frontend/tests/helperTests/apiHelper.js
+++ frontend/tests/helperTests/apiHelper.js
@@ -8,7 +8,12 @@ class Helper {
 
    this.baseUrl = "http://localhost:3001"
   }
-
+async adminLogin(adminName, adminPassword){
+ return await axios.post(`${this.baseUrl}/admin/login`,{
+  "username":adminName,
+  "password":adminPassword
+ })
+}
  async createSubAdmin(dataTable){
   const userDetails = dataTable.rowsHash();
   return await axios.post(`${this.baseUrl}/admin/`, {
@@ -35,12 +40,61 @@ class Helper {
   });
  }
 
- async updateSubAdmin(){
+ async updateSubAdmin(subAdminName, subAdminPassword){
+  const headers = {
+   "Authorization": 'Basic ' + this.encodedToken,
+   'Content-Type': 'application/json',
+  }
   return await axios.put(`${this.baseUrl}/admin/`,
       {
-       "username":"shyam",
-       "password":"shyam1"
-      })
+       "username":subAdminName,
+       "password":subAdminPassword
+      }, {headers})
+ }
+
+ async createStudent(dataTable){
+  const headers = {
+   "Authorization": 'Basic ' + this.encodedToken,
+   'Content-Type': 'application/json',
+  }
+  const studentDetails = dataTable.hashes();
+  studentCreated.push(studentDetails[0].name)
+  // console.log(studentCreated)
+  return await axios.post(`${this.baseUrl}/student/`,{
+   "name":studentDetails[0].name,
+   "email":studentDetails[0].email,
+   "phone":studentDetails[0].phone,
+   "destination":studentDetails[0].destination,
+   "qualification":studentDetails[0].qualification,
+   "address":studentDetails[0].address,
+   "percentage":studentDetails[0].percentage,
+   "ielts":studentDetails[0].ielts,
+   "reading":studentDetails[0].reading,
+   "writing":studentDetails[0].writing,
+   "listening":studentDetails[0].listening,
+   "speaking":studentDetails[0].speaking,
+   "overallband":studentDetails[0].overallband
+  }, {headers})
+ }
+
+ async deleteStudent(){
+
+ }
+
+ async listStudent(){
+  const headers = {
+   "Authorization": 'Basic ' + this.encodedToken,
+   'Content-Type': 'application/json',
+  }
+   return await axios.get(`${this.baseUrl}/student/`,{headers})
+ }
+
+ async listOneStudent(id){
+  const headers = {
+   "Authorization": 'Basic ' + this.encodedToken,
+   'Content-Type': 'application/json',
+  }
+  return await axios.get(`${this.baseUrl}/student/${id}`,{headers})
  }
 
 }
