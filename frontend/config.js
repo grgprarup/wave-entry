3d0cd5867ff859e239b322e86881d339d80a2e79
Salipa-Gurung <salipagurung@gmail.com>
2022-12-12 16:45:51 +0545

    created student cleanUp incomplete

--- frontend/config.js
+++ frontend/config.js
@@ -3,6 +3,7 @@ const { Chromium } = require("@playwright/test");
 // const deleteSubAdmin = require('./tests/helperTests/apiHelper');
 const {Helper} = require('./tests/helperTests/apiHelper')
 global.subAdminCreated = [];
+global.studentCreated = [];
 
 const helper = new Helper();
 BeforeAll(async function () {
@@ -23,6 +24,14 @@ After (async function () {
  if(subAdminCreated.length !== 0){
   for(let subAdmin of subAdminCreated ){
    await helper.deleteSubAdmin(subAdmin)
+  }
+ }
+
+ if(studentCreated.length !== 0){
+  for(let student of studentCreated){
+   if(await helper.listOneStudent(id)){
+
+   }
   }
  }
 });
